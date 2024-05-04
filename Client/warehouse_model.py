a3bd573605afb11d1334b313c8d10c7c5e0c9590
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2024-05-04 19:36:10 +0200

    update warehouse

--- Client/warehouse_model.py
+++ Client/warehouse_model.py
@@ -1,9 +1,6 @@
-from multiprocessing import Manager
-
-
 class Warehouse:
     _promo_co_10_wycen = 0
-    _original_prices = {}
+
     _products = {
         "BULKA": {"price": 5, "quantity": 100},
         "CHLEB": {"price": 5, "quantity": 100},
@@ -30,70 +27,35 @@ class Warehouse:
     @classmethod
     def PRZYWROCENIE(cls, product_name):
         cls._products[product_name]['quantity'] = 0
-        response = True
-        return response
+        return True
 
     @classmethod
     def WYCOFANIE(cls, product_name):
         cls._products[product_name]['quantity'] = -9999999
-        response = True
-        return response
+        return True
 
     @classmethod
     def POJEDYNCZE_ZAOPATRZENIE(cls, product_name):
-        current_quantity = cls._products[product_name]['quantity']
-
-        if current_quantity >= 0:
+        if cls._products[product_name]['quantity'] >= 0:
             cls._products[product_name]['quantity'] += 1
-
-            response = True
+            return True
         else:
-            response = False
-
-        return response
+            return False
 
     @classmethod
     def POJEDYNCZE_ZAMOWIENIE(cls, product_name):
-        available_quantity = cls._products[product_name]['quantity']
-
-        if available_quantity >= 1:
+        if cls._products[product_name]['quantity'] >= 1:
             cls._products[product_name]['quantity'] -= 1
-            response = True
+            return True
         else:
-            response = False
-        return response
+            return False
 
     @classmethod
     def PODAJ_CENE(cls, product_name):
         cls._promo_co_10_wycen += 1
 
         if cls._promo_co_10_wycen == 10:
-            if product_name not in cls._original_prices:
-                cls._original_prices[product_name] = cls._products[product_name]['price']
-
-            promo_price = 0
             cls._promo_co_10_wycen = 0
-        else:
-            promo_price = cls._products[product_name]['price']
+            return 0
 
-        if cls._promo_co_10_wycen == 0 and product_name in cls._original_prices:
-            cls._products[product_name]['price'] = cls._original_prices[product_name]
-            del cls._original_prices[product_name]
-
-        return promo_price
-
-    @classmethod
-    def init_manager(cls):
-        manager = Manager()
-        cls._promo_co_10_wycen = manager.Value('i', 0)
-        cls._original_prices = manager.dict()
-        product_template = {"price": 5, "quantity": 100}
-        cls._products = manager.dict({key: manager.dict(product_template) for key in cls._products.keys()})
-
-
-def main():
-    Warehouse.init_manager()
-
-
-if __name__ == '__main__':
-    main()
+        return cls._products[product_name]['price']
